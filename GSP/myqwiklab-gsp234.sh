058b00532baa084990126409f95713269606a6d7
Akshay Vasoya <161693109+AnonymousOlix@users.noreply.github.com>
2025-08-25 17:33:50 +0530

    Create myqwiklab-gsp234.sh

---
+++ GSP/myqwiklab-gsp234.sh
@@ -0,0 +1,39 @@
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                  MY QWIKLAB SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+
+mkdir sql-with-terraform
+cd sql-with-terraform
+gsutil cp -r gs://spls/gsp234/gsp234.zip .
+
+unzip gsp234.zip
+
+terraform init
+
+terraform plan -out=tfplan
+
+terraform apply tfplan
+
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                  MY QWIKLAB SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
