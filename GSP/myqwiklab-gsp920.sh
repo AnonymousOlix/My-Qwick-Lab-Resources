8b5133c37b1c96ab18b045b55b01b388bfd09c3c
Akshay Vasoya <161693109+AnonymousOlix@users.noreply.github.com>
2025-09-09 17:04:06 +0530

    Update myqwiklab-gsp920.sh

--- GSP/myqwiklab-gsp920.sh
+++ GSP/myqwiklab-gsp920.sh
@@ -136,5 +136,5 @@ echo "${GREEN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!        
 echo "${GREEN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
 
 echo
-echo "${GREEN_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@TechCode9${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@MyQwiklab${RESET_FORMAT}"
 echo
